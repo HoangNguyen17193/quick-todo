f6f46cff3852da4ebef6304c2d9c7e8ac803817d
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-10-19 09:47:16 +0700

    add task store, reducer, & saga

---
+++ store/task/taskSaga.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import {put, takeLatest, select} from 'redux-saga/effects';
+
+import {startSaveTasks, saveTaskFail, saveTaskSuccess} from "./taskAction";
+import actionTypes from './taskActionTypes';
+
+const axiosInstance = axios.create({
+    baseURL: 'https://react-todo-e217e.firebaseio.com/'
+});
+
+function* saveTasks() {
+    try {
+        yield put(startSaveTasks());
+        const state = yield select();
+        yield axiosInstance.put('/tasks.json', state.task.tasks);
+        yield put(saveTaskSuccess());
+    } catch (e) {
+        yield put(saveTaskFail(e));
+    }
+}
+
+export function* watchTask() {
+    yield takeLatest(actionTypes.SAVE_TASKS, saveTasks);
+}
